024a18da9e03c42c80e15c3400b4a76d7d3ee9f6
Yoon JiSoo <yjsgoon@naver.com>
2016-08-23 05:23:20 +0000

    set data

--- main/flask_app.py
+++ main/flask_app.py
@@ -3,14 +3,15 @@
 
 from flask import Flask
 import csv
-import sys
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def data_throw():
-    f = open('./main/dummy_log/dummy.log.csv')
-    reader = csv.DictReader(f, fieldnames = ('time', 'ID', 'x', 'y', 'speed'))
+    f = open('./main/dummy_log/scenario.log.csv')
+    reader = csv.DictReader(f, fieldnames = ('time', 'carId', 'gpsX', 'gpsY',
+                        'velocity', 'angle', 'roadType', 'heartBeat', 'iris',
+                        'CO', 'windowOffset', 'RPM', 'aircondition'))
     out = json.dumps([row for row in reader])
     return out
